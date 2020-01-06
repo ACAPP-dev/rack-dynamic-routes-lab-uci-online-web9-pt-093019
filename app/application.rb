622d461ce660f063c3611f10583019ce40469e4d
Andrew Capp <acapp909@gmail.com>
2020-01-06 22:50:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,21 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new
 
-    
+    request_item = req.path.split("/items/").last
+
+    if req.path.match(/items/)
+      if request_item == nil
+        resp.status = 400
+        resp.write "Error"
+      else
+        #write
+      end
+    else
+      resp.status = 404
+    end
+
+    resp.finish
+
 
 
   end
